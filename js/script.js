2c6d9e6c3632bdf99e0a42cc4031bcf4423995a2
Polvere243 <danielercoli@gmail.com>
2024-01-16 18:25:05 +0100

    maybe did first snack

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("JS OK");
+const tableName = 'Tavolo Vip';
+
+const guests = [
+  'Brad Pitt',
+  'Johnny Depp',
+  'Lady Gaga',
+  'Cristiano Ronaldo',
+  'Georgina Rodriguez',
+  'Chiara Ferragni',
+  'Fedez',
+  'George Clooney',
+  'Amal Clooney',
+  'Maneskin'
+];
+
+const guestsList = guests.map((guest, i) => {
+return {
+    name: guest,
+    place: i,
+    table: tableName,
+}
+})
+
+console.log(guestsList);
