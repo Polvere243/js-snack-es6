9d6ca4833b05ae9ce5ae590bf0803529309fa2a0
Polvere243 <danielercoli@gmail.com>
2024-01-16 18:44:10 +0100

    created an array of objects representing the students

--- js/students.js
+++ js/students.js
@@ -1,1 +1,37 @@
-console.log("JS OK");
+const students = [
+    {
+        ID: 213,
+        name: "Marco della Rovere",
+        vote: 78,
+    },
+    {
+        ID: 110,
+        name: "Paola Cortellessa",
+        vote: 96,
+    },
+    {
+        ID: 250,
+        name: "Andrea Mantegna",
+        vote: 48,
+    },
+    {
+        ID: 145,
+        name: "Gaia Borromini",
+        vote: 74,
+    },
+    {
+        ID: 196,
+        name: "Luigi Grimaldello",
+        vote: 68,
+    },
+    {
+        ID: 102,
+        name: "Piero della Francesca",
+        vote: 50,
+    },
+    {
+        ID: 120,
+        name: "Francesca da Polenta",
+        vote: 84,
+    }
+]
