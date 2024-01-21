81e1adfcc4e24425f8e77fc1b95ee2dab4c18b33
Polvere243 <danielercoli@gmail.com>
2024-01-21 21:47:18 +0100

    created an array of objects about bicycles

---
+++ js/bicycle.js
@@ -0,0 +1,14 @@
+const bicycles = [
+    {
+        name: "Guerciotti Veloce",
+        weight: 1.70,
+    },
+    {
+        name: "Scott Ibrida",
+        weight: 12,
+    },
+    {
+        name: "Cannondale",
+        weight: 1,
+    }
+]
