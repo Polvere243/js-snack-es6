7cd63b226db0f7e3c76a2f8cda0ecdb44da910ed
Polvere243 <danielercoli@gmail.com>
2024-01-21 23:29:31 +0100

    created random numbers for score and fouls

--- js/soccer-team.js
+++ js/soccer-team.js
@@ -11,7 +11,16 @@ const teams = [
     },
     {
         name: "Inter",
-        score:0,
+        score: 0,
         fouls: 0,
     }
 ]
+
+const getRandomNumber = (min, max) => Math.floor(Math.random () * (max + 1 - min) + min);
+
+teams.forEach (team => {
+    team.score = getRandomNumber(1, 100);
+    team.fouls = getRandomNumber(1, 100);
+})
+
+console.table(teams);
