e1bde65ddcfa3eb11cccd8721014b164b2b61148
Polvere243 <danielercoli@gmail.com>
2024-01-21 22:57:27 +0100

    created the array of objects

--- js/soccer-team.js
+++ js/soccer-team.js
@@ -1,1 +1,17 @@
-console.log("js Ok");
+const teams = [
+    {
+        name: "Juventus",
+        score: 0,
+        fouls: 0,
+    },
+    {
+        name: "Roma",
+        score: 0,
+        fouls: 0,
+    },
+    {
+        name: "Inter",
+        score:0,
+        fouls: 0,
+    }
+]
